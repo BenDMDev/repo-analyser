dedc11c6495fdc6e9c31cfac961a8ebbd2233f26
Ben Monaghan <b.monaghan1@salford.ac.uk>
2021-10-04 17:29:31 +0100

    initial project commit

---
+++ src/main/java/com/redocelot/RepoAnalyser/RepoAnalyserApplication.java
@@ -0,0 +1,13 @@
+package com.redocelot.RepoAnalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoAnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoAnalyserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redocelot/RepoAnalyserTests/RepoAnalyserTests.java
@@ -0,0 +1,13 @@
+package com.redocelot.RepoAnalyserTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoAnalyserTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
